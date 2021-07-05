a9150f02d3b169b612bd241e07d5e5c3f8a47f2c
Lucas Lopes <lslopes1903@gmail.com>
2021-07-05 15:31:49 -0300

    API mudanças

--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -1,4 +1,4 @@
 // const API = 'https://wnooeu0fcj.execute-api.sa-east-1.amazonaws.com/API_PROJETOFACUL'
-const API = 'http://backendprojetofacul.ddns.net:3001'
+const API = 'http://projetofacul.ddns.net:3001'
 
 module.exports = API
