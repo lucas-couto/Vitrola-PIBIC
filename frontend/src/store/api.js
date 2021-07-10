d3ef954b5a161e32672628800c8e9650a04a5b32
Lucas Lopes <lslopes1903@gmail.com>
2021-07-09 21:00:22 -0300

    Nova url de API

--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -1,4 +1,4 @@
-// const API = 'https://wnooeu0fcj.execute-api.sa-east-1.amazonaws.com/API_PROJETOFACUL'
-const API = 'http://localhost:3001'
+const API = 'https://wnooeu0fcj.execute-api.sa-east-1.amazonaws.com/API_PROJETOFACUL'
+
 
 module.exports = API
