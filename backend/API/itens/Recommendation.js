458dca49dd2d08fe53a6b09fafed561a5a175f40
Lucas Lopes <lslopes1903@gmail.com>
2021-07-12 11:23:29 -0300

    Mudanças no Recommendation.js

--- backend/API/itens/Recommendation.js
+++ backend/API/itens/Recommendation.js
@@ -7,70 +7,61 @@ let musicInfo
 let musicMbid
 let musicName
 let musicYoutubeUrl
-let musicDirectoryImage 
+let musicDirectoryImage
 let musicAlbumName
 let musicAlbumMbid
 let musicArtistName
 let musicArtistMbid
 let prom
-let exist
+let musics
 async function getAllRecommendation(paramsPlaylistMusics) {
     allPlaylistMbidMusics = JSON.parse(paramsPlaylistMusics)
-    allRecommendationMusics = []
+    allRecommendationMusics.length = 0
     for (let i = 0; i < allPlaylistMbidMusics.length; i++) {
-        allSimilarMusicsMbid = []
-        await Musics_Similar.findAll({ where: { music_mbid: allPlaylistMbidMusics[i] }, order: [['similarityScore', 'DESC']] })
-            .then(async musics => {
-                for (let i = 0; i < musics.length; i++) {
-                    if (!await ola(musics[i].dataValues.similarMusic_mbid)) {
-                        allSimilarMusicsMbid.push({mbid:musics[i].dataValues.similarMusic_mbid, similarityScore: musics[i].dataValues.similarityScore})
-                    }
-                }
-
-            })
+        allSimilarMusicsMbid.length = 0
+        musics = await Musics_Similar.findAll({ where: { music_mbid: allPlaylistMbidMusics[i] }, order: [['similarityScore', 'DESC']] })
+        for (let i = 0; i < musics.length; i++) {
+            if (!await discardExistingPlaylistMusics(musics[i].dataValues.similarMusic_mbid, allPlaylistMbidMusics)) {
+                allSimilarMusicsMbid.push({ mbid: musics[i].dataValues.similarMusic_mbid, similarityScore: musics[i].dataValues.similarityScore })
+            }
+        }
     }
+    await orderRecommendationMusics(allSimilarMusicsMbid)
     await getTop10Musics(allSimilarMusicsMbid)
-    await getAllMusics(allSimilarMusicsMbid)
-    await prom
     return allRecommendationMusics
 }
 
-async function getAllMusics(allSimilarMusicsMbid) {
-    prom = new Promise(async (resolve, reject) => {
-        for (let i = 0; i < 10; i++) {
-            musicInfo = await getAllMusicInformation(allSimilarMusicsMbid[i].mbid)
-            musicMbid = musicInfo.music.musicMbid
-            musicName = musicInfo.music.musicName
-            musicYoutubeUrl = musicInfo.music.musicYoutubeUrl
-            musicDirectoryImage = musicInfo.music.musicImage
-            musicAlbumName = musicInfo.album.albumName
-            musicAlbumMbid = musicInfo.album.albumMbid
-            musicArtistName = musicInfo.artist.artistName
-            musicArtistMbid = musicInfo.artist.artistMbid
-            allRecommendationMusics.push({
-                musicMbid,
-                musicName, musicYoutubeUrl,
-                musicDirectoryImage, musicAlbumName,
-                musicAlbumMbid, musicArtistName,
-                musicArtistMbid
-            })
-            if (i == allSimilarMusicsMbid.length - 1)
-                resolve()
-        }
-    })
-}
-
-async function ola(param) {
-    exist = false
-    for (let i = 0; i < allRecommendationMusics.length; i++) {
-        if (param == allSimilarMusicsMbid[i].similarMusicMbid)
-            exist = true
+async function getTop10Musics(allSimilarMusicsMbid) {
+    for (let i = 0; i < allSimilarMusicsMbid.length; i++) {
+        musicInfo = await getAllMusicInformation(allSimilarMusicsMbid[i].mbid)
+        musicMbid = musicInfo.music.musicMbid
+        musicName = musicInfo.music.musicName
+        musicYoutubeUrl = musicInfo.music.musicYoutubeUrl
+        musicDirectoryImage = musicInfo.music.musicImage
+        musicAlbumName = musicInfo.album.albumName
+        musicAlbumMbid = musicInfo.album.albumMbid
+        musicArtistName = musicInfo.artist.artistName
+        musicArtistMbid = musicInfo.artist.artistMbid
+        allRecommendationMusics.push({
+            musicMbid,
+            musicName, musicYoutubeUrl,
+            musicDirectoryImage, musicAlbumName,
+            musicAlbumMbid, musicArtistName,
+            musicArtistMbid
+        })
     }
-    return exist
 }
 
-async function getTop10Musics(allSimilarMusicsMbid){
-    allSimilarMusicsMbid.sort((a,b) =>(a.similarityScore > b.similarityScore) ? 1 : ((b.similarityScore > a.similarityScore) ? -1 : 0))
+async function discardExistingPlaylistMusics(similarMusicMbid, allPlaylistMbidMusics) {
+    for (let i = 0; i <= allPlaylistMbidMusics.length; i++) {
+        if (similarMusicMbid == allPlaylistMbidMusics[i])
+            return true
+    }
+    return false
+}
+
+async function orderRecommendationMusics(allSimilarMusicsMbid) {
+    allSimilarMusicsMbid.sort((a, b) => (a.similarityScore > b.similarityScore) ? 1 : ((b.similarityScore > a.similarityScore) ? -1 : 0))
 }
 
 async function Recommendation(req, res) {
@@ -78,5 +69,6 @@ async function Recommendation(req, res) {
     paramsPlaylistMusics = req.params.playlistMusics
     res.json(await getAllRecommendation(paramsPlaylistMusics))
 }
+
 
 module.exports = { Recommendation }
