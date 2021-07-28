4c7718cb915a659c669fa6c27d245d722387a1c2
lucas-couto <lslopes1903@hotmail.com>
2021-07-28 16:17:08 -0300

    trocando url de api

--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -1,4 +1,5 @@
-const API = 'https://wnooeu0fcj.execute-api.sa-east-1.amazonaws.com/API_PROJETOFACUL'
+// const API = 'https://wnooeu0fcj.execute-api.sa-east-1.amazonaws.com/API_PROJETOFACUL'
+const API = 'https://vitrola.sytes.net:3001'
 
 
 module.exports = API
